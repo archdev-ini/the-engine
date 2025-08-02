3b9a70cbecd9ea3b6d2d8d660c61f06d848d6924
Inioluwa Oladipupo <officialinioluwaoladipupo@gmail.com>
2025-08-02 07:19:25 +0000

    cange the favicon to the logo that is adapdatble with the light and dark

---
+++ src/app/icon.tsx
@@ -0,0 +1,49 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          borderRadius: '50%',
+          backgroundColor: '#09090b',
+        }}
+      >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#fafafa"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M12 8V4H8"></path>
+            <rect width="16" height="12" x="4" y="8" rx="2"></rect>
+            <path d="M2 14h2"></path>
+            <path d="M20 14h2"></path>
+            <path d="M15 13v2"></path>
+            <path d="M9 13v2"></path>
+        </svg>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
